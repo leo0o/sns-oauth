c2f345b6e962e7da369abc07639e66152e84b5cd
leooo <281970273@qq.com>
2019-12-24 16:49:34 +0800

    update

---
+++ src/SNSOAuth/Common/Platform.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: leooo.
+ * Date: 2019/12/24
+ * Time: 16:46
+ */
+
+namespace SNSOAuth\Common;
+
+
+final class Platform
+{
+
+    // 微信
+    const  WECHAT = 'weixin';
+
+    // qq
+    const  QQ = 'qq';
+
+    // 新浪微博
+    const  SINA_WEIBO = 'weibo';
+
+}
